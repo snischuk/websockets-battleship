62580a7203018dc50fd18430253645859d535e26
Max Snischuk <m.snischuk@gmail.com>
2025-11-12 13:05:49 +0200

    chore: add .env.example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import { config } from 'dotenv';
+config();
+
 import { httpServer } from './http_server/index';
 
-const HTTP_PORT = 8181;
+const PORT = process.env.PORT || 3000;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+console.log(`Start static http server on the ${PORT} port!`);
+httpServer.listen(PORT);
